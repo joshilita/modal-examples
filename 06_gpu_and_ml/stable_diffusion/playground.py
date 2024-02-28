ddf85fc2d413b0066e80a97b842094da7d70c05c
Akshat Bubna <akshatb42@gmail.com>
2024-02-28 01:32:59 +0000

    h100

--- 06_gpu_and_ml/stable_diffusion/playground.py
+++ 06_gpu_and_ml/stable_diffusion/playground.py
@@ -24,7 +24,7 @@ with image.imports():
     from fastapi import Response
 
 
-@stub.cls(image=image, gpu="a100", keep_warm=1)
+@stub.cls(image=image, gpu="h100", keep_warm=1)
 class Model:
     @modal.build()
     @modal.enter()
