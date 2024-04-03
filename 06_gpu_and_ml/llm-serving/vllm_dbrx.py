1a9ec52d9871ec81aec188d9016e4d4bd0ce1a4d
Charles Frye <charles@modal.com>
2024-04-03 22:09:25 +0000

    adds very jank version of DBRX serving

---
+++ 06_gpu_and_ml/llm-serving/vllm_dbrx.py
@@ -0,0 +1,260 @@
+# # Fast inference with vLLM (DBRX)
+#
+# In this example, we show how to run basic inference, using [`vLLM`](https://github.com/vllm-project/vllm)
+# to take advantage of PagedAttention, which speeds up sequential inferences with optimized key-value caching.
+#
+# We are running the [Mixtral 8x7B Instruct](https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1) model here,
+# which is a mixture-of-experts model finetuned for conversation.
+# You can expect ~3 minute cold starts.
+# For a single request, the throughput is over 50 tokens/second.
+# The larger the batch of prompts, the higher the throughput (up to hundreds of tokens per second).
+#
+# ## Setup
+#
+# First we import the components we need from `modal`.
+
+import os
+import time
+
+from modal import Image, Secret, Stub, enter, exit, gpu, method
+
+MODEL_DIR = "/model"
+BASE_MODEL = "databricks/dbrx-instruct"
+GPU_CONFIG = gpu.A100(memory=80, count=4)
+
+
+# ## Define a container image
+#
+# We want to create a Modal image which has the model weights pre-saved to a directory. The benefit of this
+# is that the container no longer has to re-download the model from Huggingface - instead, it will take
+# advantage of Modal's internal filesystem for faster cold starts.
+#
+# ### Download the weights
+#
+# We can download the model to a particular directory using the HuggingFace utility function `snapshot_download`.
+#
+# Mixtral is beefy, at nearly 100 GB in `safetensors` format, so this can take some time -- at least a few minutes.
+#
+# Tip: avoid using global variables in this function. Changes to code outside this function will not be detected and the download step will not re-run.
+def download_model_to_folder():
+    from huggingface_hub import snapshot_download
+    from transformers.utils import move_cache
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    snapshot_download(
+        BASE_MODEL,
+        local_dir=MODEL_DIR,
+        ignore_patterns=["*.pt"],  # Using safetensors
+    )
+    move_cache()
+
+
+# ### Image definition
+# We’ll start from a Dockerhub image recommended by `vLLM`, and use
+# run_function to run the function defined above to ensure the weights of
+# the model are saved within the container image.
+
+vllm_image = (
+    Image.from_registry(
+        "nvidia/cuda:12.1.1-devel-ubuntu22.04", add_python="3.10"
+    )
+    .pip_install(
+        "https://github.com/vllm-project/vllm/releases/download/v0.4.0/vllm-0.4.0-cp310-cp310-manylinux1_x86_64.whl",  # vllm==0.4.0
+        "huggingface_hub==0.19.4",
+        "hf-transfer==0.1.4",
+        "transformers~=4.39.2",
+        "tiktoken==0.6.0",
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+    .run_function(
+        download_model_to_folder,
+        timeout=60 * 60,
+        secrets=[Secret.from_name("huggingface-secret")],
+    )
+)
+
+stub = Stub("example-vllm-dbrx")
+
+
+# ## The model class
+#
+# The inference function is best represented with Modal's [class syntax](/docs/guide/lifecycle-functions) and the `@enter` decorator.
+# This enables us to load the model into memory just once every time a container starts up, and keep it cached
+# on the GPU for each subsequent invocation of the function.
+#
+# The `vLLM` library allows the code to remain quite clean. We do have to patch the multi-GPU setup due to issues with Ray.
+@stub.cls(
+    gpu=GPU_CONFIG,
+    timeout=20 * 60,
+    container_idle_timeout=60 * 20,
+    allow_concurrent_inputs=10,
+    image=vllm_image,
+    keep_warm=1,
+)
+class Model:
+    @enter()
+    def start_engine(self):
+        import time
+
+        from vllm.engine.arg_utils import AsyncEngineArgs
+        from vllm.engine.async_llm_engine import AsyncLLMEngine
+
+        print("🥶 cold starting inference")
+        start = time.monotonic_ns()
+
+        if GPU_CONFIG.count > 1:
+            # Patch issue from https://github.com/vllm-project/vllm/issues/1116
+            import ray
+
+            ray.shutdown()
+            ray.init(num_gpus=GPU_CONFIG.count, ignore_reinit_error=True)
+
+        engine_args = AsyncEngineArgs(
+            model=MODEL_DIR,
+            tensor_parallel_size=GPU_CONFIG.count,
+            gpu_memory_utilization=0.95,
+            max_model_len=4096,
+            enforce_eager=False,  # capture the graph for faster inference, but slower cold starts
+            disable_log_stats=True,  # disable logging so we can stream tokens
+            disable_log_requests=True,
+            trust_remote_code=BASE_MODEL
+            == "databricks/dbrx-instruct",  # models not yet upstreamed to transformers require this
+        )
+        self.template = "<s> [INST] {user} [/INST] "
+
+        # this can take some time!
+        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
+        duration_s = (time.monotonic_ns() - start) / 1e9
+        print(f"🏎️ engine started in {duration_s:.0f}s")
+
+    @method()
+    async def completion_stream(self, user_question):
+        from vllm import SamplingParams
+        from vllm.utils import random_uuid
+
+        sampling_params = SamplingParams(
+            temperature=0.75,
+            max_tokens=128,
+            repetition_penalty=1.1,
+        )
+
+        request_id = random_uuid()
+        result_generator = self.engine.generate(
+            self.template.format(user=user_question),
+            sampling_params,
+            request_id,
+        )
+        index, num_tokens = 0, 0
+        start = time.monotonic_ns()
+        async for output in result_generator:
+            if (
+                output.outputs[0].text
+                and "\ufffd" == output.outputs[0].text[-1]
+            ):
+                continue
+            text_delta = output.outputs[0].text[index:]
+            index = len(output.outputs[0].text)
+            num_tokens = len(output.outputs[0].token_ids)
+
+            yield text_delta
+        duration_s = (time.monotonic_ns() - start) / 1e9
+
+        yield f"\n\tGenerated {num_tokens} tokens from {BASE_MODEL} in {duration_s:.1f}s, throughput = {num_tokens / duration_s:.0f} tokens/second on {GPU_CONFIG}.\n"
+
+    @exit()
+    def stop_engine(self):
+        if GPU_CONFIG.count > 1:
+            import ray
+
+            ray.shutdown()
+
+
+# ## Run the model
+# We define a [`local_entrypoint`](/docs/guide/apps#entrypoints-for-ephemeral-apps) to call our remote function
+# sequentially for a list of inputs. You can run this locally with `modal run -q vllm_mixtral.py`. The `q` flag
+# enables the text to stream in your local terminal.
+@stub.local_entrypoint()
+def main():
+    model = Model()
+    questions = [
+        "Implement a Python function to compute the Fibonacci numbers.",
+        "What is the fable involving a fox and grapes?",
+        "What were the major contributing factors to the fall of the Roman Empire?",
+        "Describe the city of the future, considering advances in technology, environmental changes, and societal shifts.",
+        "What is the product of 9 and 8?",
+        "Who was Emperor Norton I, and what was his significance in San Francisco's history?",
+    ]
+    for question in questions:
+        print("Sending new request:", question, "\n\n")
+        for text in model.completion_stream.remote_gen(question):
+            print(text, end="", flush=True)
+
+
+# ## Deploy and invoke the model
+# Once we deploy this model with `modal deploy text_generation_inference.py`,
+# we can invoke inference from other apps, sharing the same pool
+# of GPU containers with all other apps we might need.
+#
+# ```
+# $ python
+# >>> import modal
+# >>> f = modal.Function.lookup("example-tgi-Mixtral-8x7B-Instruct-v0.1", "Model.generate")
+# >>> f.remote("What is the story about the fox and grapes?")
+# 'The story about the fox and grapes ...
+# ```
+
+# ## Coupling a frontend web application
+#
+# We can stream inference from a FastAPI backend, also deployed on Modal.
+#
+# You can try our deployment [here](https://modal-labs--vllm-mixtral.modal.run).
+
+from pathlib import Path
+
+from modal import Mount, asgi_app
+
+frontend_path = Path(__file__).parent.parent / "llm-frontend"
+
+
+@stub.function(
+    mounts=[Mount.from_local_dir(frontend_path, remote_path="/assets")],
+    keep_warm=1,
+    allow_concurrent_inputs=20,
+    timeout=60 * 10,
+)
+@asgi_app(label="vllm-dbrx")
+def app():
+    import json
+
+    import fastapi
+    import fastapi.staticfiles
+    from fastapi.responses import StreamingResponse
+
+    web_app = fastapi.FastAPI()
+
+    @web_app.get("/stats")
+    async def stats():
+        stats = await Model().completion_stream.get_current_stats.aio()
+        return {
+            "backlog": stats.backlog,
+            "num_total_runners": stats.num_total_runners,
+            "model": BASE_MODEL + " (vLLM)",
+        }
+
+    @web_app.get("/completion/{question}")
+    async def completion(question: str):
+        from urllib.parse import unquote
+
+        async def generate():
+            async for text in Model().completion_stream.remote_gen.aio(
+                unquote(question)
+            ):
+                yield f"data: {json.dumps(dict(text=text), ensure_ascii=False)}\n\n"
+
+        return StreamingResponse(generate(), media_type="text/event-stream")
+
+    web_app.mount(
+        "/", fastapi.staticfiles.StaticFiles(directory="/assets", html=True)
+    )
+    return web_app
